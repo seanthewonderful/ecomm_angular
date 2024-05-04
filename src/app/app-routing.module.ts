182b3296184f138d9e3fbfe647a97fb4c13f3266
Sean Fagan <faganseanp@gmail.com>
2024-05-04 12:42:35 -0600

    practice adding modules and components with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./modules/admin/admin.module').then(
+        (module) => module.AdminModule
+      ),
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/user/user.module').then((module) => module.UserModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
